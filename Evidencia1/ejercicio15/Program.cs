62236a7cd70d2e4f90a3feea517de5b176e1c428
Angel-Diaz-H <130614039+Angel-Diaz-H@users.noreply.github.com>
2025-03-10 10:30:44 -0600

    ejercicio9

--- Evidencia1/ejercicio15/Program.cs
+++ Evidencia1/ejercicio15/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//15. Realizar un programa que convierta de dólares a pesos. 
+
+Console.WriteLine("Programa para convertir dólares a pesos.")
