6b5e9a28af3c83aa054becdcd02266b613968720
Angel-Diaz-H <130614039+Angel-Diaz-H@users.noreply.github.com>
2025-03-10 10:15:08 -0600

    Ejercicio10

--- Evidencia1/ejercicio10/Program.cs
+++ Evidencia1/ejercicio10/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//10. Realizar un programa que calcule el área de un círculo.
+
+Console.WriteLine("Programa para calcular el área de un círculo");
+
+//Entrada, proceso y salida.
+static double entrada(){
+    Console.Write("Ingresa el radio del círculo: ");
+    double Pnum1 = Convert.ToDouble(Console.ReadLine());
+    return Pnum1;
+}
+
+static double proceso(double radio){
+    // área = pi * r ^ 2
+    return Math.PI * radio * radio;
+}
+
+static void MostrarResultado( double PArea){
+    Console.WriteLine($"El área del círculo es: {PArea}.");
+}
+
+double radio = entrada();
+double area = proceso(radio);
+MostrarResultado(area);
+
+Console.ReadKey();
