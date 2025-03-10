d99d61fe8012045d1e131064c512874794a78694
Angel-Diaz-H <130614039+Angel-Diaz-H@users.noreply.github.com>
2025-03-10 21:03:04 +0000

    Por_tema

---
+++ EjerciciosComplementarios/LearnListasYArrays1/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Descripción:
+Lista de nombres
+
+Crea una lista de nombres.
+Agrega al menos 5 nombres.
+Imprime todos los nombres.
+Pregunta al usuario un nombre, si sí agregarlo.
+Eliminar el primer nombre.
+Vuelve a imprimir la lista actualizada.
+*/
+
+List<string> LNombres = new List<string>();
+
+for (int i = 1; i<=5; i++){
+	Console.Write($"Ingresa el nombre {i}: ");
+	string Nombre = Console.ReadLine();
+	LNombres.Add(Nombre);
+}
+
+Console.WriteLine("Los nombres ingresados son: ");
+foreach (string Nombre in LNombres){
+	Console.Write($"[{Nombre}]");
+}
+
+Console.WriteLine();
+
+Console.Write("¿Desea agregar un nombre? (S/N): ");
+
+if (Convert.ToChar(Console.ReadLine().ToUpper()) == 'S'){
+	Console.Write("Ingrese el nombre: ");
+	string Nombre = Console.ReadLine();
+    LNombres.Add(Nombre);
+}
+
+LNombres.RemoveAt(0);
+
+Console.WriteLine("¡Se ha eliminado el primer nombre y se ha agregado uno nuevo!");
+
+Console.WriteLine("Lista de nombres actualizados:");
+
+foreach (string nombre in LNombres){
+	Console.Write($"[{nombre}]");
+}
+
+Console.ReadKey();
--- Evidencia1/MenuConversiones/Program.cs
+++ Evidencia1/MenuConversiones/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*15. Realizar un programa que convierta de dólares a pesos. 
+16. Realizar un programa que convierta de dólares a euros. 
+17. Realizar un programa que convierta de Pesos a dólares. 
+18. Realizar un programa que convierta de Pesos a euros. 
+19. Realizar un programa que convierta de euros a pesos. 
+20. Realizar un programa que convierta de euros a dólares.*/
+
+//Entrada, proceso y salida.
+namespace ConvertirMonedas(){
+
+    class programa(){
+        static void Main(){
+            
+        }
+    }
+}
