1ef9a9e59e6b649a0df19fb47a614f829283382d
Angel-Diaz-H <130614039+Angel-Diaz-H@users.noreply.github.com>
2025-05-07 09:26:17 -0600

    ejercicio1

--- Evidencia2/ejercicio1/Program.cs
+++ Evidencia2/ejercicio1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Realizar un programa que me permita mostrar en la pantalla lo siguiente: 
+a.	Tu nombre es (nombre completo).
+b.	Tu edad es.
+c.	Tu dirección es.
+*/
+
+string Nombre, Edad, Direccion;
+
+Console.WriteLine("Ingresa tu nombre completo: ");
+Nombre = Console.ReadLine();
+
+Console.WriteLine("Ingresa tu edad: ");
+Edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresa tu dirección: ");
+Direccion = Console.ReadLine();
+
+Console.WriteLine("");
+Console.WriteLine($"Tu nombre es: {Nombre}");
+Console.WriteLine($"Tu edad es: {Edad}");
+Console.WriteLine($"Tu dirección es: {Direccion}");
