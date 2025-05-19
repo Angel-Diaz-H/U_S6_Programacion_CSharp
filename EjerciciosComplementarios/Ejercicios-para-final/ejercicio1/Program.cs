2ab4b97cfeb3d7e31aed14ecc28854f581b2b74c
Angel-Diaz-H <130614039+Angel-Diaz-H@users.noreply.github.com>
2025-05-19 01:43:48 +0000

    ejercicios para examen final

---
+++ EjerciciosComplementarios/Ejercicios-para-final/ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Suma de dos números enteros.
+
+static int RecibeNumeros(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+static void SolicitarDatos()
+{
+    Console.WriteLine($"Ingresa el primer número: ");
+    int num1 = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Ingresa el segundo número: ");
+    int num2 = int.Parse(Console.ReadLine());
+
+    int Resultado = RecibeNumeros(num1, num2);
+
+    Console.WriteLine($"La suma de {num1} y {num2} es: {Resultado}");
+}
